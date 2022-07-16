1aa0be2bbe73f65f831bb7607539521b3f36235b
talaatmagdyx <talaatmagdy75@gmail.com>
2022-07-16 23:09:45 +0200

    add collection for all objects

--- lib/vultr.rb
+++ lib/vultr.rb
@@ -4,6 +4,11 @@ require_relative "vultr/version"
 
 module Vultr
   autoload :Client, "vultr/client"
+  autoload :Collection, "vultr/collection"
   autoload :Object, "vultr/object"
   autoload :Error, "vultr/error"
+
+  autoload :Account, "vultr/objects/account"
+  autoload :Instance, "vultr/objects/instance"
+  autoload :Plan, "vultr/objects/plan"
 end
---
+++ lib/vultr/collection.rb
@@ -0,0 +1,21 @@
+module Vultr
+  class Collection
+    attr_reader :data, :total, :next_cursor, :prev_cursor
+
+    # from_response(response, key: "applications", type: Application) type mean class name
+    def self.from_response(response, key:, type:)
+      body = response.body
+      new(data: body[key].map{ |attrs| type.new(attrs) },
+          total: body.dig('meta', 'total'),
+          next_cursor: body.dig('meta', 'links', 'next'),
+          prev_cursor: body.dig('meta', 'links', 'prev'))
+    end
+
+    def initialize(data:, total:, next_cursor:, prev_cursor:)
+      @data = data
+      @total = total
+      @next_cursor = next_cursor.empty? ? nil : next_cursor
+      @prev_cursor = prev_cursor.empty? ? nil : prev_cursor
+    end
+  end
+end
