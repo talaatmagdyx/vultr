4a7ce62d78d9e2b6e64607feba48e7eb62d4179d
talaatmagdyx <talaatmagdy75@gmail.com>
2022-07-16 21:50:14 +0200

    add client class to initialize and make connection using faraday

--- lib/vultr/client.rb
+++ lib/vultr/client.rb
@@ -1,5 +1,27 @@
+require 'faraday'
+
 module Vultr
   class Client
+    BASE_URL = 'https://api.vultr.com/v2/'
+    attr_reader :api_key, :adapter
+
+    def initialize(api_key:, adapter: Faraday.default_adapter)
+      @api_key = api_key
+      @adapter = adapter
+    end
+
+    def connection
+      @connection ||= Faraday.new do |conn|
+        conn.url_prefix = BASE_URL
+        conn.request :json
+        conn.response :json, content_type: 'application/json'
+        conn.adapter @adapter
+      end
+    end
+
+    def inspect
+      "#<Vultr::Client>"
+    end
   end
 end
 
